c3f543ad99656c6fc22d4bf7a438a5d0bfd46334
jslok <jslok930@gmail.com>
2023-11-10 11:57:47 -0800

    'second'

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import { View, Button, UIManager, LayoutAnimation, Text } from 'react-native';
-import Animated from 'react-native-reanimated';
+import React, { useState } from 'react'
+import { View, Button, UIManager, LayoutAnimation, Text } from 'react-native'
+import Animated from 'react-native-reanimated'
 
 if (Platform.OS === 'android') {
   if (UIManager.setLayoutAnimationEnabledExperimental) {
-    UIManager.setLayoutAnimationEnabledExperimental(true);
+    UIManager.setLayoutAnimationEnabledExperimental(true)
   }
 }
 
 const Home: React.FC = ({ navigation }) => {
-  const [bigSquare, setBigSquare] = useState(false);
+  const [bigSquare, setBigSquare] = useState(false)
 
   return (
     <View style={{ flex: 1 }}>
       <Button
         title="Do LayoutAnimation (change size)"
         onPress={() => {
-          LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-          setBigSquare(!bigSquare);
+          LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+          setBigSquare(!bigSquare)
         }}
       />
       <View
@@ -25,15 +25,16 @@ const Home: React.FC = ({ navigation }) => {
           width: bigSquare ? 250 : 150,
           height: bigSquare ? 250 : 150,
           backgroundColor: 'green',
-        }}>
+        }}
+      >
         <Text>Resize expected to be animated</Text>
       </View>
 
-      <Animated.View sharedTransitionTag={'sometag'}>
+      <Animated.View style={{ backgroundColor: 'yellow' }} sharedTransitionTag={'sometag'}>
         <Text>Animated compomnent w/ sharedTransitionTag</Text>
       </Animated.View>
     </View>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
